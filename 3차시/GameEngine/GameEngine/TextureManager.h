ad4339931c033ecfc5192aef176cedab3cc9a343
iyaho <dlwlsgh0808@gmail.com>
2020-08-12 17:24:13 +0900

    ddd
    dd

---
+++ 3차시/GameEngine/GameEngine/TextureManager.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <d3dx9.h>
+#include <map>
+
+class TextureManager
+{
+private:
+	std::map<char*, LPDIRECT3DTEXTURE9> textureMap;
+public:
+	TextureManager();
+	~TextureManager();
+	
+	LPDIRECT3DTEXTURE9 LoadTextureFromFile(char* path);
+
+
+};
+
